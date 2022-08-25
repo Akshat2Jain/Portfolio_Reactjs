19317bc132587e27b62c2d5f51df72d6f3d15020
akshat jain <akshat.2024it1031@kiet.edu>
2022-08-25 13:44:24 +0530

    added a new blog

--- src/component/BlogApi.js
+++ src/component/BlogApi.js
@@ -1,36 +1,39 @@
 const blogList = [
-    {
-      id: 1,
-      title: "What is 2 Min Blog? ",
-      category: "Introduction",
-      subCategory: ["Intro", "WebDev"],
-      description:
-        "The idea of creating the (2 min blog)  is to just put my thoughts on topics related to tech or any other random topics that will take literally  2 mins or less than 2  mins to read. According to Google, the average wpm(word per minute) of an average person is 150-200. This means that a person can read around 150-200 words per minute. So keeping in mind this analysis, I got an idea why not let's create a blog website where I can put blogs that will exactly contain 400-500 words which is a lot and still takes 2 mins to read.",
-      description2:
-        "The blogs will be on categories like technology, web development, or just random thoughts that give me a sudden overwhelming feeling at the midnight to talk to someone.As this is just a starting, I will be the only author for the (2 min blog) but as we go further I will take blogs from other people too.",
-      footer:"See Ya in Glory 😊",
-        authorName: "Akshat Jain",
-      authorAvatar: "/assets/images/author.jpg",
-      createdAt: "August 22 , 2022",
-      cover: "/assets/images/que-es-un-blog-1.jpeg",
-      link:"https://2minblog.vercel.app/blog/1"
-    },
-    {
-        id: 2,
-        title: "What is 2 Min Blog? ",
-        category: "Introduction",
-        subCategory: ["Intro", "WebDev"],
-        description:
-          "The idea of creating the (2 min blog)  is to just put my thoughts on topics related to tech or any other random topics that will take literally  2 mins or less than 2  mins to read. According to Google, the average wpm(word per minute) of an average person is 150-200. This means that a person can read around 150-200 words per minute. So keeping in mind this analysis, I got an idea why not let's create a blog website where I can put blogs that will exactly contain 400-500 words which is a lot and still takes 2 mins to read.",
-        description2:
-          "The blogs will be on categories like technology, web development, or just random thoughts that give me a sudden overwhelming feeling at the midnight to talk to someone.As this is just a starting, I will be the only author for the (2 min blog) but as we go further I will take blogs from other people too.",
-        footer:"See Ya in Glory 😊",
-          authorName: "Akshat Jain",
-        authorAvatar: "/assets/images/author.jpg",
-        createdAt: "August 22 , 2022",
-        cover: "/assets/images/que-es-un-blog-1.jpeg",
-        link:"https://2minblog.vercel.app/blog/1"
-      },
-  ];
-  
-  export default blogList;
+  {
+    id: 1,
+    title: "What is 2 Min Blog? ",
+    category: "Introduction",
+    subCategory: ["Intro", "WebDev"],
+    description:
+      "The idea of creating the (2 min blog)  is to just put my thoughts on topics related to tech or any other random topics that will take literally  2 mins or less than 2  mins to read. According to Google, the average wpm(word per minute) of an average person is 150-200. This means that a person can read around 150-200 words per minute. So keeping in mind this analysis, I got an idea why not let's create a blog website where I can put blogs that will exactly contain 400-500 words which is a lot and still takes 2 mins to read.",
+    description2:
+      "The blogs will be on categories like technology, web development, or just random thoughts that give me a sudden overwhelming feeling at the midnight to talk to someone.As this is just a starting, I will be the only author for the (2 min blog) but as we go further I will take blogs from other people too.",
+    footer: "See Ya in Glory 😊",
+    authorName: "Akshat Jain",
+    authorAvatar: "/assets/images/author.jpg",
+    createdAt: "August 22 , 2022",
+    cover: "/assets/images/que-es-un-blog-1.jpeg",
+    link: "https://2minblog.vercel.app/blog/1",
+  },
+  {
+    id: 2,
+    title: "Why should Todo be your 1st project in Learning Development?",
+    category: "Tech",
+    subCategory: ["Tech", "WebDev"],
+    description:
+      "Have you ever thought why there are more than 1000+ tutorials on youtube, teaching you how to make a to-do list whether, in Html CSS js or in react, vue, angular or any other frameworks/libraries present in the market? It's because it gives you a basic understanding of the language.How language works. Let's understand technically why I and many other successful developers suggest that todo should be your 1st project learning development.",
+    description2:
+      "For eg, creating todo you will learn about the crud operations which means create, read, update and delete. You create a list, read it from the array or any backend server, update that list and delete that list. In the first project, you will learn how to work with arrays in that particular language.How to work with the same CRUD operations in an array. Learn about the basic operations that you can do using javascript like creating functions, working with variables, working with the objects and many other things. You will get a touch of how to work with web development tools like console, how to work with the local storage and session storage etc. Learning all those things will give you great hands-on the language as a beginner.",
+    description3:
+      "So Go ahead and create your ToDo list. The best resource I can tell you is youtube only. As I told there are so many videos on youtube, try to understand the concept instead of just copying it. Tell us your experience reading The 2min blog. Share this 2min blog with your friends,colleagues etc",
+    footer: "See Ya in Glory 😊",
+    authorName: "Akshat Jain",
+    authorAvatar: "/assets/images/author.jpg",
+    createdAt: "August 24 , 2022",
+    cover: "/assets/images/blo2.jpg",
+    url: window.location.href,
+    link:"https://2minblog.vercel.app/blog/2"
+  },
+];
+
+export default blogList;
